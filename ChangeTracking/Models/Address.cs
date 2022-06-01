2a012977f1d8b94811b33448e2a3b8eb0368902e
Christopher Ziemacki <52426440+christopher-ziemacki@users.noreply.github.com>
2022-06-01 22:14:16 +0300

    Simple change tracking for POCO using reflection.

---
+++ ChangeTracking/Models/Address.cs
@@ -0,0 +1,17 @@
+namespace ChangeTracking.Models;
+
+public class Address
+{
+    public string Street { get; set; }
+    public string City { get; set; }
+    public string PostalCode { get; set; }
+    public string Country { get; set; }
+
+    public Address(string street, string city, string postalCode, string country)
+    {
+        Street = street;
+        City = city;
+        PostalCode = postalCode;
+        Country = country;
+    }
+}
--- ChangeTracking/Program.cs
+++ ChangeTracking/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Reflection;
+using ChangeTracking.Models;
 
-Console.WriteLine("Hello, World!");
+var person = new Person("Gregory", "Wisneski");
+
+var personType = typeof(Person);
+
+var properties = personType.GetProperties();
+
+var snapshots = properties.Select(p => new PropertySnapshot(p, person)).ToArray();
+
+person.FirstName = "John";
+
+foreach (var snapshot in snapshots)
+{
+    var currentValue = snapshot.Property.GetValue(person);
+    if (currentValue != snapshot.Value)
+    {
+        Console.WriteLine($"'{snapshot.Property.Name}' property value has changed." +
+                          $" Previous value was '{snapshot.Value}'." +
+                          $" Current value is '{currentValue}'.");
+    }
+}
+
+class PropertySnapshot
+{
+    public PropertyInfo Property { get; }
+    public object Target { get; }
+    public object Value { get; }
+ 
+    public PropertySnapshot(PropertyInfo property, object target)
+    {
+        Property = property;
+        Target = target;
+        
+        Value = property.GetValue(target);
+    }
+}
+
+
+
